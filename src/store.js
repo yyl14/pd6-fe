b9560b69efc6523fb558eace78a8ec45708ee835
gary1030 <gary6658@ntu.im>
2021-07-07 23:31:46 +0800

    fix some compile problem but not completely solved

--- src/store.js
+++ src/store.js
@@ -1,6 +1,5 @@
 import thunk from 'redux-thunk';
+import { createStore, applyMiddleware } from 'redux';
 import reducer from './reducers';
 
-import { createStore, applyMiddleware } from 'redux';
-
-export const store = createStore(reducer, applyMiddleware(thunk));
+export default createStore(reducer, applyMiddleware(thunk));
