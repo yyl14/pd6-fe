92cb8e413b60fe62da522bd2e200695b5cd528c8
timicienio <timmy960072@gmail.com>
2021-07-08 13:38:35 +0800

    solved eslint problem

--- src/reportWebVitals.js
+++ src/reportWebVitals.js
@@ -1,12 +1,12 @@
+import { getLCP, getFID, getCLS, getFCP, getTTFB } from 'web-vitals';
+
 const reportWebVitals = (onPerfEntry) => {
   if (onPerfEntry && onPerfEntry instanceof Function) {
-    import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
-      getCLS(onPerfEntry);
-      getFID(onPerfEntry);
-      getFCP(onPerfEntry);
-      getLCP(onPerfEntry);
-      getTTFB(onPerfEntry);
-    });
+    getCLS(onPerfEntry);
+    getFID(onPerfEntry);
+    getFCP(onPerfEntry);
+    getLCP(onPerfEntry);
+    getTTFB(onPerfEntry);
   }
 };
 
