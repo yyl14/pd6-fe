dc888927ba36ef58743466e67438c5a7ba74b0a2
timicienio <timmy960072@gmail.com>
2021-07-11 15:16:16 +0800

    Relative font size, default font size changed to 18

--- src/theme.js
+++ src/theme.js
@@ -33,9 +33,13 @@ const palette = {
 
 const typography = {
   // UI/UX terminology
+
+  // Defualt font size, basis of 'rem' units
+  fontSize: 18,
+
   // Big Title
   h1: {
-    fontSize: 48,
+    fontSize: '2.4rem',
     fontWeight: 700,
     lineHeight: 57 / 48,
     letterSpacing: '-0.01em',
@@ -44,7 +48,7 @@ const typography = {
 
   // Title
   h2: {
-    fontSize: 24,
+    fontSize: '1.2rem',
     fontWeight: 600,
     lineHeight: 29 / 24,
     letterSpacing: '-0.01em',
@@ -53,7 +57,7 @@ const typography = {
 
   // Body
   body1: {
-    fontSize: 20,
+    fontSize: '1rem',
     fontWeight: 500,
     lineHeight: 24 / 20,
     letterSpacing: '-0.01em',
@@ -62,7 +66,7 @@ const typography = {
 
   // Sub-body
   body2: {
-    fontSize: 16,
+    fontSize: '0.8rem',
     fontWeight: 400,
     lineHeight: 19 / 16,
     letterSpacing: '-0.01em',
@@ -71,7 +75,7 @@ const typography = {
 
   // Caption
   caption: {
-    fontSize: 14,
+    fontSize: '0.7rem',
     fontWeight: 400,
     lineHeight: 17 / 14,
     letterSpacing: '-0.02em',
@@ -80,7 +84,7 @@ const typography = {
 
   button: {
     textTransform: 'none',
-    fontSize: 20,
+    fontSize: '1rem',
     lineHeight: 24 / 20,
     letterSpacing: '-0.01em',
     fontFamily: 'San Francisco',
@@ -134,7 +138,7 @@ const overrides = {
   // "Input"
   MuiTextField: {
     root: {
-      margin: '1vh 5px 2vh 5px',
+      margin: '1vh 5px 2.2vh 5px',
     },
   },
   MuiOutlinedInput: {
@@ -146,7 +150,7 @@ const overrides = {
       '& input': {
         padding: '12px 0px 14px 15px',
         fontWeight: 500,
-        fontSize: '20px',
+        fontSize: '1rem',
       },
     },
   },
@@ -164,7 +168,7 @@ const overrides = {
   MuiInputLabel: {
     outlined: {
       top: '-5px',
-      '&$shrink': { transform: 'translate(14px, -11px) scale(0.70)', fontWeight: 400 },
+      '&$shrink': { transform: 'translate(14px, -10px) scale(0.80)', fontWeight: 400 },
     },
   },
 
