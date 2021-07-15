4375fa30f8b447a4a569653aac8af439466e6a43
timicienio <timmy960072@gmail.com>
2021-07-16 01:39:25 +0800

    Font family, typography, input label, error text

--- src/components/ErrorText.js
+++ src/components/ErrorText.js
@@ -3,12 +3,12 @@ import { FormHelperText, Typography } from '@material-ui/core';
 import { MdError } from 'react-icons/md';
 
 export default function ErrorText({ children, className }) {
-  return children ? (
-    <div className={className}>
-      <MdError style={{ fontSize: '0.8rem', transform: 'translate(0px, -1px) ' }} />
-      <Typography variant="body2">{children}</Typography>
-    </div>
-  ) : (
-    <></>
+  return (
+    children && (
+      <div className={className}>
+        <MdError style={{ fontSize: '0.8rem' }} />
+        <Typography variant="body2">{children}</Typography>
+      </div>
+    )
   );
 }
