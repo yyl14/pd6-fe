b2669a6d5a3e48162cee6dea17a5e5a6a9be9268
timicienio <timmy960072@gmail.com>
2021-07-24 14:29:21 +0800

    course reducer

--- src/reducers/admin/course.js
+++ src/reducers/admin/course.js
@@ -3,12 +3,16 @@ import { courseConstants } from '../../actions/constant';
 const initialState = {
   courses: {
     byId: {},
-    allIds: {},
+    allIds: [],
+    error: null,
+    loading: false,
   },
 
   classes: {
     byId: {},
-    allIds: {},
+    allIds: [],
+    error: null,
+    loading: false,
   },
 
   // challenges: {
@@ -18,7 +22,9 @@ const initialState = {
 
   members: {
     byId: {},
-    allIds: {},
+    allIds: [],
+    error: null,
+    loading: false,
   },
 
   // teams: {
@@ -31,3 +37,77 @@ const initialState = {
   //   allIds: {},
   // },
 };
+
+export default function course(state = initialState, action) {
+  switch (action.type) {
+    // Course
+
+    case courseConstants.FETCH_COURSES_START:
+      return {
+        ...state,
+        courses: { ...state.courses, loading: true },
+      };
+
+    case courseConstants.FETCH_COURSES_SUCCESS: {
+      const { data } = action.payload;
+      return {
+        ...state,
+        courses: {
+          byId: data.reduce((acc, item) => ({ ...acc, [item.key]: item }), {}),
+          allIds: data.map((item) => item.id),
+          error: null,
+          loading: false,
+        },
+      };
+    }
+
+    case courseConstants.FETCH_COURSES_FAIL: {
+      const { error } = action.payload;
+      return {
+        ...state,
+        courses: {
+          byId: {},
+          allIds: [],
+          error,
+          loading: false,
+        },
+      };
+    }
+
+    case courseConstants.FETCH_COURSES_START:
+      return {
+        ...state,
+        courses: { ...state.courses, loading: true },
+      };
+
+    case courseConstants.FETCH_COURSES_SUCCESS: {
+      const { data } = action.payload;
+      return {
+        ...state,
+        courses: {
+          byId: data.reduce((acc, item) => ({ ...acc, [item.key]: item }), {}),
+          allIds: data.map((item) => item.id),
+          error: null,
+          loading: false,
+        },
+      };
+    }
+
+    case courseConstants.FETCH_COURSES_FAIL: {
+      const { error } = action.payload;
+      return {
+        ...state,
+        courses: {
+          byId: {},
+          allIds: [],
+          error,
+          loading: false,
+        },
+      };
+    }
+
+    default: {
+      return state;
+    }
+  }
+}
