fd78b879bd3f0be337fbec4394142dfcde2213a8
gary1030 <gary6658@ntu.im>
2021-07-27 17:37:26 +0800

    update account reducer

--- src/reducers/admin/account.js
+++ src/reducers/admin/account.js
@@ -1,23 +1,47 @@
 import { accountConstants } from '../../actions/constant';
 
 const initialState = {
-  institute: {
-    institutes: [],
-    error: null,
-    loading: false,
+  institutes: {
+    byId: {},
+    allIds: [],
   },
 
   account: {
-    info: {},
-    error: null,
-    loading: false,
+    byId: {},
+    allIds: [],
   },
 
   studentCard: {
     byId: {},
     allIds: [],
-    error: null,
-    loading: false,
+  },
+
+  loading: {
+    fetchInstitute: false,
+    addInstitute: false,
+    editInstitute: false,
+
+    fetchAccount: false,
+    editAccount: false,
+    deleteAccount: false,
+
+    fetchStudentCard: false,
+    addStudentCard: false,
+    makeStudentCardDefault: false,
+  },
+
+  error: {
+    fetchInstitute: null,
+    addInstitute: null,
+    editInstitute: null,
+
+    fetchAccount: null,
+    editAccount: null,
+    deleteAccount: null,
+
+    fetchStudentCard: null,
+    addStudentCard: null,
+    makeStudentCardDefault: null,
   },
 };
 
@@ -27,31 +51,262 @@ export default function account(state = initialState, action) {
     case accountConstants.FETCH_INSTITUTE_REQUEST:
       return {
         ...state,
-        institute: { ...state.institute, loading: true },
+        loading: {
+          ...state.loading,
+          fetchInstitute: true,
+        },
       };
     case accountConstants.FETCH_INSTITUTE_SUCCESS:
       return {
         ...state,
-        institute: { institutes: action.payload, error: null, loading: false },
+        institutes: {
+          byId: action.payload.reduce((acc, item) => ({ ...acc, [item.id]: { ...item } }), state.institutes),
+          allIds: action.payload.map((item) => item.id),
+        },
+        loading: {
+          ...state.loading,
+          fetchInstitute: false,
+        },
+        error: {
+          ...state.error,
+          fetchInstitute: null,
+        },
       };
     case accountConstants.FETCH_INSTITUTE_FAIL:
       return {
         ...state,
-        institute: { ...state.institute, error: action.error, loading: false },
+        loading: {
+          ...state.loading,
+          fetchInstitute: false,
+        },
+        error: {
+          ...state.error,
+          fetchInstitute: action.error,
+        },
+      };
+    case accountConstants.ADD_INSTITUTE_REQUEST:
+      return {
+        ...state,
+        loading: {
+          ...state.loading,
+          addInstitute: true,
+        },
       };
     case accountConstants.ADD_INSTITUTE_SUCCESS:
       return {
         ...state,
-        institute: { institutes: [...state.institute.institutes, action.payload], error: action.error, loading: false },
+        institutes: {
+          ...state.institutes,
+          byId: { [action.payload.id]: action.payload },
+          allIds: state.institutes.allIds.concat([[action.payload.id]]),
+        },
+        loading: {
+          ...state.loading,
+          addInstitute: false,
+        },
+        error: {
+          ...state.error,
+          addInstitute: null,
+        },
       };
     case accountConstants.ADD_INSTITUTE_FAIL:
       return {
         ...state,
-        institute: { ...state.institute, error: action.error, loading: false },
+        loading: {
+          ...state.loading,
+          addInstitute: false,
+        },
+        error: {
+          ...state.error,
+          addInstitute: action.error,
+        },
+      };
+    case accountConstants.EDIT_INSTITUTE_REQUEST:
+      return {
+        ...state,
+        loading: {
+          ...state.loading,
+          editInstitute: true,
+        },
+      };
+    case accountConstants.EDIT_INSTITUTE_SUCCESS:
+      return {
+        ...state,
+        institutes: { ...state.institutes, [action.payload.id]: action.payload },
+        loading: {
+          ...state.loading,
+          editInstitute: false,
+        },
+        error: {
+          ...state.error,
+          editInstitute: null,
+        },
+      };
+    case accountConstants.EDIT_INSTITUTE_FAIL:
+      return {
+        ...state,
+        loading: {
+          ...state.loading,
+          addInstitute: false,
+        },
+        error: {
+          ...state.error,
+          addInstitute: action.error,
+        },
       };
 
-      // account
+    // account
+    case accountConstants.FETCH_ACCOUNT_REQUEST:
+      return {
+        ...state,
+        loading: {
+          ...state.loading,
+          fetchAccount: true,
+        },
+      };
+    case accountConstants.FETCH_ACCOUNT_SUCCESS:
+      return {
+        ...state,
+        account: {
+          byId: action.payload.reduce((acc, item) => ({ ...acc, [item.id]: { ...item } }), state.institutes),
+          allIds: action.payload.map((item) => item.id),
+        },
+        loading: {
+          ...state.loading,
+          fetchAccount: false,
+        },
+        error: {
+          ...state.error,
+          fetchAccount: null,
+        },
+      };
+    case accountConstants.FETCH_ACCOUNT_FAIL:
+      return {
+        ...state,
+        loading: {
+          ...state.loading,
+          fetchAccount: false,
+        },
+        error: {
+          ...state.error,
+          fetchAccount: action.error,
+        },
+      };
+    case accountConstants.EDIT_ACCOUNT_REQUEST:
+      return {
+        ...state,
+        loading: {
+          ...state.loading,
+          editAccount: true,
+        },
+      };
+    case accountConstants.EDIT_ACCOUNT_SUCCESS: {
+      const editedAccount = state.account.byId[action.payload.id];
+      editedAccount.nickname = action.payload.nickname;
+      editedAccount.alternative_email = action.payload.alternative_email;
+      return {
+        ...state,
+        account: { ...state.account, [action.payload.id]: editedAccount },
+        loading: {
+          ...state.loading,
+          editAccount: false,
+        },
+        error: {
+          ...state.error,
+          editAccount: null,
+        },
+      };
+    }
+    case accountConstants.EDIT_ACCOUNT_FAIL:
+      return {
+        ...state,
+        loading: {
+          ...state.loading,
+          editAccount: false,
+        },
+        error: {
+          ...state.error,
+          editAccount: action.error,
+        },
+      };
+    case accountConstants.DELETE_ACCOUNT_REQUEST:
+      return {
+        ...state,
+        loading: {
+          ...state.loading,
+          deleteAccount: true,
+        },
+      };
+    case accountConstants.DELETE_ACCOUNT_SUCCESS: {
+      const newById = { ...state.account.byId };
+      delete newById[action.payload.id];
+      return {
+        ...state,
+        account: {
+          ...state.account,
+          byId: newById,
+          allIds: state.account.allIds.filter((item) => item !== action.payload.id),
+        },
+        loading: {
+          ...state.loading,
+          deleteAccount: false,
+        },
+        error: {
+          ...state.error,
+          deleteAccount: null,
+        },
+      };
+    }
+    case accountConstants.DELETE_ACCOUNT_FAIL:
+      return {
+        ...state,
+        loading: {
+          ...state.loading,
+          deleteAccount: false,
+        },
+        error: {
+          ...state.error,
+          deleteAccount: action.error,
+        },
+      };
 
+    // student card
+    case accountConstants.MAKE_STUDENT_CARD_DEFAULT_REQUEST:
+      return {
+
+      };
+    case accountConstants.MAKE_STUDENT_CARD_DEFAULT_SUCCESS:
+      return {
+
+      };
+    case accountConstants.MAKE_STUDENT_CARD_DEFAULT_FAIL:
+      return {
+
+      };
+    case accountConstants.FETCH_STUDENT_CARD_REQUEST:
+      return {
+
+      };
+    case accountConstants.FETCH_STUDENT_CARD_SUCCESS:
+      return {
+
+      };
+    case accountConstants.FETCH_STUDENT_CARD_FAIL:
+      return {
+
+      };
+    case accountConstants.ADD_STUDENT_CARD_REQUEST:
+      return {
+
+      };
+    case accountConstants.ADD_STUDENT_CARD_SUCCESS:
+      return {
+
+      };
+    case accountConstants.ADD_STUDENT_CARD_FAIL:
+      return {
+
+      };
     default:
       return state;
   }
