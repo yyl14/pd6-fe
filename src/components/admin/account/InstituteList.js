ca70cb06fcfd071a11189e05918b5b3b6164e8c6
gary1030 <gary6658@ntu.im>
2021-08-02 12:37:21 +0800

    update institute list page

--- src/components/admin/account/InstituteList.js
+++ src/components/admin/account/InstituteList.js
@@ -1,8 +1,20 @@
 import React, { useState, useEffect } from 'react';
-import { useSelector } from 'react-redux';
-import { makeStyles, Button, Typography } from '@material-ui/core';
+import { useSelector, useDispatch } from 'react-redux';
+import {
+  makeStyles, Button, Typography, Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+  TextField,
+  FormControlLabel,
+  Switch,
+} from '@material-ui/core';
 import { useParams } from 'react-router-dom';
+import { bindActionCreators } from 'redux';
 import CustomTable from '../../ui/CustomTable';
+import FieldWithAlignedText from '../../ui/FieldWithAlignedText';
+import { accountActions } from '../../../actions/index';
 
 const useStyles = makeStyles((theme) => ({
   pageHeader: {
@@ -12,12 +24,39 @@ const useStyles = makeStyles((theme) => ({
 
 export default function InstituteList() {
   const classes = useStyles();
+  const dispatch = useDispatch();
+  const { addInstitute } = bindActionCreators(accountActions, dispatch);
 
   const institutes = useSelector((state) => state.admin.account.institutes.byId);
   const institutesID = useSelector((state) => state.admin.account.institutes.allIds);
+  const cookies = useSelector((state) => state.cookies);
 
   const [tableData, setTableData] = useState([]);
   const [path, setPath] = useState([]);
+
+  const [popUp, setPopUp] = useState(false);
+  const [error, setError] = useState(false);
+  const [errorText, setErrorText] = useState('');
+  const [inputs, setInputs] = useState({
+    fullName: '',
+    initialism: '',
+    email: '',
+    status: false,
+  });
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setInputs((input) => ({ ...input, [name]: value }));
+  };
+
+  const handleChangeStatus = (event) => {
+    setInputs((input) => ({ ...input, [event.target.name]: event.target.checked }));
+  };
+
+  const add = () => {
+    addInstitute('', inputs.initialism, inputs.fullName, inputs.email, inputs.status);
+    console.log('hello');
+  };
 
   useEffect(() => {
     const newData = [];
@@ -46,7 +85,7 @@ export default function InstituteList() {
         searchPlaceholder="Institute/Email"
         buttons={(
           <>
-            <Button color="primary">Add Institute</Button>
+            <Button color="primary" onClick={() => setPopUp(true)}>Add Institute</Button>
           </>
         )}
         data={tableData}
@@ -70,11 +109,69 @@ export default function InstituteList() {
             align: 'center',
           },
         ]}
-        hasFilter={[false, false, true]}
+        hasFilter={[false, false, false]}
         dataColumnName={['full_name', 'email_domain', 'is_disabled']}
         hasLink
         path={path}
       />
+      <Dialog
+        open={popUp}
+        keepMounted
+        onClose={() => setPopUp(false)}
+        aria-labelledby="dialog-slide-title"
+        aria-describedby="dialog-slide-description"
+      >
+        <DialogTitle id="dialog-slide-title">
+          <Typography variant="h4">Add Institute</Typography>
+        </DialogTitle>
+        <DialogContent>
+          <FieldWithAlignedText text="Full Name">
+            <TextField
+              id="fullName"
+              name="fullName"
+              placeholder="e.g. National Taiwan University"
+              value={inputs.fullName}
+              onChange={(e) => handleChange(e)}
+              error={error}
+              helperText={errorText}
+            />
+          </FieldWithAlignedText>
+          <FieldWithAlignedText text="Initialism">
+            <TextField
+              id="initialism"
+              name="initialism"
+              placeholder="e.g. NTU"
+              value={inputs.initialism}
+              onChange={(e) => handleChange(e)}
+            />
+          </FieldWithAlignedText>
+          <FieldWithAlignedText text="Email">
+            <TextField
+              id="email"
+              name="email"
+              placeholder="e.g. ntu.edu.tw"
+              value={inputs.email}
+              onChange={(e) => handleChange(e)}
+            />
+          </FieldWithAlignedText>
+          <FieldWithAlignedText text="Initialism">
+            <FormControlLabel
+              control={
+                <Switch checked={inputs.status} onChange={handleChangeStatus} name="status" color="primary" />
+            }
+              label={inputs.status ? 'Enabled' : 'Disabled'}
+            />
+          </FieldWithAlignedText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={() => setPopUp(false)} color="disabled">
+            Cancel
+          </Button>
+          <Button onClick={() => { setPopUp(false); add(); }} color="primary">
+            Add
+          </Button>
+        </DialogActions>
+      </Dialog>
     </>
   );
 }
