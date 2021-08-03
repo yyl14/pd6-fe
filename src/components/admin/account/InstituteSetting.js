eadd1840427858dd3dac2f2905d72d9c76457b8c
gary1030 <gary6658@ntu.im>
2021-08-03 16:54:05 +0800

    update institute setting

--- src/components/admin/account/InstituteSetting.js
+++ src/components/admin/account/InstituteSetting.js
@@ -23,13 +23,42 @@ const useStyles = makeStyles((theme) => ({
   pageHeader: {
     marginBottom: '50px',
   },
+  wrapper: {
+    display: 'flex',
+    flexDirection: 'row',
+    verticalAlign: 'center',
+  },
+  alignedTextWrapper1: {
+    width: '190px',
+  },
+  alignedTextWrapper2: {
+    width: '600px',
+  },
+  alignedText: {
+    marginTop: '10px',
+    marginBottom: '10px',
+  },
+  textWrapper1: {
+    width: '200px',
+    color: 'red',
+  },
+  textWrapper2: {
+    width: '400px',
+    color: 'red',
+  },
+  textWrapper3: {
+    width: '200px',
+  },
+  textWrapper4: {
+    width: '400px',
+  },
 }));
 
 export default function InstituteSetting() {
   const classes = useStyles();
 
   const { instituteId } = useParams();
-  // const institutes = useSelector((state) => state.admin.account.institutes.byId);
+  const institutes = useSelector((state) => state.admin.account.institutes.byId);
 
   const [settingStatus, setSettingStatus] = useState({
     changeName: false,
@@ -56,6 +85,7 @@ export default function InstituteSetting() {
 
   const handleChange = (prop) => (event) => {
     setNewSetting((input) => ({ ...input, [prop]: event.target.value }));
+    console.log(instituteId);
   };
 
   const handleChangeStatus = (event) => {
@@ -65,12 +95,59 @@ export default function InstituteSetting() {
   return (
     <>
       <Typography variant="h3" className={classes.pageHeader}>
-        NTU / Setting
+        {`${institutes[instituteId].abbreviated_name} / Setting`}
       </Typography>
       <SimpleBar
         title="Institute Information"
       >
-        <AlignedText text="Full Name" />
+        <div className={classes.wrapper}>
+          <div className={classes.alignedTextWrapper1}>
+            <Typography variant="body1" className={classes.alignedText}>
+              Full Name
+            </Typography>
+          </div>
+          <div className={classes.alignedTextWrapper2}>
+            <Typography variant="body1" className={classes.alignedText}>
+              {institutes[instituteId].full_name}
+            </Typography>
+          </div>
+        </div>
+        <div className={classes.wrapper}>
+          <div className={classes.alignedTextWrapper1}>
+            <Typography variant="body1" className={classes.alignedText}>
+              Initialism
+            </Typography>
+          </div>
+          <div className={classes.alignedTextWrapper2}>
+            <Typography variant="body1" className={classes.alignedText}>
+              {institutes[instituteId].abbreviated_name}
+            </Typography>
+          </div>
+        </div>
+        <div className={classes.wrapper}>
+          <div className={classes.alignedTextWrapper1}>
+            <Typography variant="body1" className={classes.alignedText}>
+              Email
+            </Typography>
+          </div>
+          <div className={classes.alignedTextWrapper2}>
+            <Typography variant="body1" className={classes.alignedText}>
+              {institutes[instituteId].email_domain}
+            </Typography>
+          </div>
+        </div>
+        <div className={classes.wrapper}>
+          <div className={classes.alignedTextWrapper1}>
+            <Typography variant="body1" className={classes.alignedText}>
+              Status
+            </Typography>
+          </div>
+          <div className={classes.alignedTextWrapper2}>
+            <Typography variant="body1" className={classes.alignedText}>
+              {institutes[instituteId].is_disabled ? 'Disabled' : 'Enabled'}
+            </Typography>
+          </div>
+        </div>
       </SimpleBar>
       <SimpleBar
         title="Change Institute Full Name"
@@ -157,26 +234,32 @@ export default function InstituteSetting() {
         onClose={() => handleClosePopUp()}
         aria-labelledby="dialog-slide-title"
         aria-describedby="dialog-slide-description"
+        fullWidth
+        maxWidth="sm"
       >
         <DialogTitle id="dialog-slide-title">
           <Typography variant="h4">Rename institute</Typography>
         </DialogTitle>
         <DialogContent>
-          <Grid container spacing={3}>
-            <Grid item xs={5}>
-              <Typography variant="body1" style={{ color: 'red' }}>
-                Current Name
+          <div className={classes.wrapper}>
+            <div className={classes.textWrapper1}>
+              <Typography variant="body1" className={classes.alignedText}>
+                Full Name
               </Typography>
-            </Grid>
-            <Grid item xs={7}>
-              <Typography variant="body1" style={{ color: 'red' }}>
-                National Taiwan University
+            </div>
+            <div className={classes.textWrapper2}>
+              <Typography variant="body1" className={classes.alignedText}>
+                {institutes[instituteId].full_name}
               </Typography>
-            </Grid>
-            <Grid item xs={5}>
-              <Typography variant="body1">New Name</Typography>
-            </Grid>
-            <Grid item xs={7}>
+            </div>
+          </div>
+          <div className={classes.wrapper}>
+            <div className={classes.textWrapper3}>
+              <Typography variant="body1" className={classes.alignedText}>
+                New Name
+              </Typography>
+            </div>
+            <div className={classes.textWrapper4}>
               <OutlinedInput
                 id="outlined-adornment"
                 value={newSetting.newName}
@@ -186,9 +269,10 @@ export default function InstituteSetting() {
                   'aria-label': 'newName',
                 }}
                 labelWidth={0}
+                style={{ width: 360 }}
               />
-            </Grid>
-          </Grid>
+            </div>
+          </div>
           <Typography variant="body1">
             Once you change the institute’s name, all related members will be affected. Please be certain.
           </Typography>
@@ -208,6 +292,8 @@ export default function InstituteSetting() {
         onClose={() => handleClosePopUp()}
         aria-labelledby="dialog-slide-title"
         aria-describedby="dialog-slide-description"
+        fullWidth
+        maxWidth="sm"
       >
         <DialogTitle id="dialog-slide-title">
           <Typography variant="h4">Change institute Initialism</Typography>
@@ -221,7 +307,7 @@ export default function InstituteSetting() {
             </Grid>
             <Grid item xs={7}>
               <Typography variant="body1" style={{ color: 'red' }}>
-                NTU
+                {institutes[instituteId].abbreviated_name}
               </Typography>
             </Grid>
             <Grid item xs={5}>
@@ -259,6 +345,8 @@ export default function InstituteSetting() {
         onClose={() => handleClosePopUp()}
         aria-labelledby="dialog-slide-title"
         aria-describedby="dialog-slide-description"
+        fullWidth
+        maxWidth="sm"
       >
         <DialogTitle id="dialog-slide-title">
           <Typography variant="h4">Rename institute</Typography>
