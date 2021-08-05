65513513ec77438901541ae500e403520cfb8b65
wilson <wilsonhsukdnhc@gmail.com>
2021-08-05 23:02:22 +0800

    compiled error.

--- src/components/admin/system/AnnouncementHome.js
+++ src/components/admin/system/AnnouncementHome.js
@@ -1,11 +1,12 @@
 import React, { Component, useState, useEffect } from 'react';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import {
   withRouter, Switch, Route, useHistory, useParams, BrowserRouter as Router, Link,
 } from 'react-router-dom';
 import { BiFilterAlt } from 'react-icons/bi';
 import { Button, Typography, makeStyles } from '@material-ui/core';
 import CustomTable from '../../ui/CustomTable';
+import { fetchAnnouncement } from '../../../actions/admin/system';
 
 const useStyles = makeStyles((theme) => ({
   pageHeader: {
@@ -23,24 +24,34 @@ const useStyles = makeStyles((theme) => ({
 export default function AnnouncementHome() {
   const classes = useStyles();
 
-  const announcement = useSelector((state) => state.admin.system.announcements.byId);
-  const announcementID = useSelector((state) => state.admin.system.announcements.allIds);
+  const dispatch = useDispatch();
+  const authToken = useSelector((state) => state.auth.user.token);
+  const loading = useSelector((state) => state.admin.system.loading.fetchAnnouncement);
+  const announcements = useSelector((state) => state.admin.system.announcements.byId);
+  const announcementId = useSelector((state) => state.admin.system.announcements.allIds);
 
   const [tableData, setTableData] = useState([]);
   const [path, setPath] = useState([]);
 
   useEffect(() => {
+    dispatch(fetchAnnouncement(authToken));
+    console.log('call fetch announcement : ', announcements);
+  }, [announcements, authToken, dispatch]);
+
+  useEffect(() => {
     const newData = [];
     const newPath = [];
 
-    announcementID.forEach((key) => {
-      const item = announcement[key];
-      newData.push(item);
+    announcementId.forEach((key) => {
+      const item = announcements[key];
+      console.log('item', item);
+      const temp = { ...item };
+      newData.push(temp);
       newPath.push(`announcement/${item.id}/setting`);
     });
     setTableData(newData);
     setPath(newPath);
-  }, [announcement, announcementID]);
+  }, [announcements, announcementId]);
 
   const history = useHistory();
   const handleClickAdd = () => {
@@ -48,30 +59,6 @@ export default function AnnouncementHome() {
   };
 
   const [filter, setFilter] = useState(false);
-
-  const columns = [
-    {
-      id: 'title',
-      label: 'Title',
-      minWidth: 200,
-      width: 200,
-      align: 'center',
-    },
-    {
-      id: 'PostTime',
-      label: 'Post Time',
-      minWidth: 150,
-      width: 150,
-      align: 'center',
-    },
-    {
-      id: 'EndTime',
-      label: 'End Time',
-      minWidth: 150,
-      width: 150,
-      align: 'center',
-    },
-  ];
 
   return (
     <>
@@ -87,7 +74,29 @@ export default function AnnouncementHome() {
           </Button>
             )}
         data={tableData}
-        columns={columns}
+        columns={[
+          {
+            id: 'title',
+            label: 'Title',
+            minWidth: 200,
+            width: 200,
+            align: 'center',
+          },
+          {
+            id: 'PostTime',
+            label: 'Post Time',
+            minWidth: 150,
+            width: 150,
+            align: 'center',
+          },
+          {
+            id: 'EndTime',
+            label: 'End Time',
+            minWidth: 150,
+            width: 150,
+            align: 'center',
+          },
+        ]}
         columnComponent={[null, (<BiFilterAlt key="filter" onClick={() => { setFilter(true); }} />), (<BiFilterAlt key="filter" onClick={() => { setFilter(true); }} />)]}
         hasFilter={[false, true, true]}
         dataColumnName={['Title', 'PostTime', 'EndTime']}
