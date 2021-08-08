01b3ed06d49aa050bdbf497020096b1d87345376
gary1030 <gary6658@ntu.im>
2021-08-08 21:42:04 +0800

    phase init draft

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,10 +3,22 @@ import globalError from './globalError';
 import auth from './auth';
 import admin from './admin/index';
 import publicState from './public';
+import challenge from './challenge';
+import grade from './grade';
+import member from './member';
+import problem from './problem';
+import submission from './submission';
+import team from './team';
 
 export default combineReducers({
   globalError,
   auth,
   admin,
   publicState,
+  challenge,
+  grade,
+  member,
+  problem,
+  submission,
+  team,
 });
