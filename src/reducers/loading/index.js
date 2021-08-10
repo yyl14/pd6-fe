bcc47443d25ffaaf5f5c5e4ba8ef4f51daadb536
gary1030 <gary6658@ntu.im>
2021-08-10 11:36:38 +0800

    store bugs fixed

--- src/reducers/loading/index.js
+++ src/reducers/loading/index.js
@@ -1,6 +1,6 @@
 import { combineReducers } from 'redux';
 import admin from './admin';
-import myClass from './myClass';
+// import myClass from './myClass';
 import common from './common';
 
-export default combineReducers({ admin, myClass, common });
+export default combineReducers({ admin, common });
