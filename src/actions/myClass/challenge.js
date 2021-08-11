a229364d56a47206768ff64e3a103e42ed30ee58
am8liu <am8liu@ntu.im>
2021-08-11 18:12:11 +0800

    error fixed

--- src/actions/myClass/challenge.js
+++ src/actions/myClass/challenge.js
@@ -26,7 +26,7 @@ const fetchChallenges = (token, classId) => (dispatch) => {
     });
 };
 
-const addChallenge = (token, classId) => (dispatch) => {
+const addChallenge = (token, classId, type, publicizeType, title, description, startTime, endTime) => (dispatch) => {
   const auth = {
     headers: {
       'Auth-Token': token,
@@ -34,7 +34,15 @@ const addChallenge = (token, classId) => (dispatch) => {
   };
   dispatch({ type: challengeConstants.ADD_CHALLENGE_REQUEST });
 
-  agent.post(`/class/${classId}/challenge`, auth)
+  agent.post(`/class/${classId}/challenge`, {
+    class_id: classId,
+    type,
+    publicize_type: publicizeType,
+    title,
+    description,
+    start_time: startTime,
+    end_time: endTime,
+  }, auth)
     .then((res) => {
       dispatch({
         type: challengeConstants.ADD_CHALLENGE_SUCCESS,
