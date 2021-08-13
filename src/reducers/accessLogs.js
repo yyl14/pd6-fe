acbd7f2d8e76c5ba9ed2e49002cad3f43f529e58
raymanlee89 <raymanlee89@gmail.com>
2021-08-13 20:39:30 +0800

    Fix untracked files problem.

---
+++ src/reducers/accessLogs.js
@@ -0,0 +1,26 @@
+import { combineReducers } from 'redux';
+import { systemConstants } from '../actions/admin/constants';
+
+const byId = (state = {}, action) => {
+  switch (action.type) {
+    case systemConstants.FETCH_ACCESS_LOG_SUCCESS: {
+      const data = Object.values(action.payload);
+      return data.reduce((acc, item) => ({ ...acc, [item.id]: { ...item } }), state.logs);
+    }
+    default:
+      return state;
+  }
+};
+
+const allIds = (state = [], action) => {
+  switch (action.type) {
+    case systemConstants.FETCH_ACCESS_LOG_SUCCESS: {
+      const data = Object.values(action.payload);
+      return data.map((item) => item.id);
+    }
+    default:
+      return state;
+  }
+};
+
+export default combineReducers({ byId, allIds });
