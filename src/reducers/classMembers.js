84928e449cb425a39f70cf679011a569a314eeac
timicienio <timmy960072@gmail.com>
2021-08-16 03:08:27 +0800

    System role transformation

--- src/reducers/classMembers.js
+++ src/reducers/classMembers.js
@@ -4,9 +4,8 @@ import { commonConstants } from '../actions/common/constant';
 const byId = (state = {}, action) => {
   switch (action.type) {
     case commonConstants.FETCH_CLASS_MEMBERS_SUCCESS: {
-      console.log('fetch member success');
       const { data } = action.payload;
-      return data.reduce((acc, item) => ({ ...acc, [item.member_id]: item }), state);
+      return data.reduce((acc, item) => ({ ...acc, [item.member_id]: { ...item } }), state);
     }
     default:
       return state;
