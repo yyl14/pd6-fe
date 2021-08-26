1101fa6072c46364c397b6ee3fc8d956d35af878
Daphne <daphne@ntu.im>
2021-08-26 08:54:35 +0800

    bug fixed

--- src/containers/auth/RegisterForm.js
+++ src/containers/auth/RegisterForm.js
@@ -83,7 +83,6 @@ export default function RegisterForm() {
     realName: false,
     school: false,
     username: false,
-    nickname: false,
     studentId: false,
     email: false,
     password: false,
@@ -93,7 +92,6 @@ export default function RegisterForm() {
     realName: '',
     school: '',
     username: '',
-    nickname: '',
     studentId: '',
     email: '',
     password: '',
@@ -108,7 +106,7 @@ export default function RegisterForm() {
   const [showPassword, setShowPassword] = useState(false);
   const [showConfirmPassword, setShowConfirmPassword] = useState(false);
 
-  const labelName = ['realName', 'school', 'username', 'nickname', 'studentId', 'email', 'password', 'confirmPassword'];
+  const labelName = ['realName', 'school', 'username', 'studentId', 'email', 'password', 'confirmPassword'];
 
   useEffect(() => {
     dispatch(getInstitutes());
