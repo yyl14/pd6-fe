6b2e4d99645c85aad9d4d8bc98110cc17447e3d9
gary1030 <gary6658@ntu.im>
2021-08-27 16:28:55 +0800

    adjustment

--- src/reducers/problems.js
+++ src/reducers/problems.js
@@ -1,5 +1,5 @@
 import { combineReducers } from 'redux';
-import { problemConstants } from '../actions/myClass/constant';
+import { problemConstants, submissionConstants } from '../actions/myClass/constant';
 import { commonConstants } from '../actions/common/constant';
 
 const byId = (state = {}, action) => {
