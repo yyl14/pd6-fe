fc3344a2842eb1ace5a76868a4e43dd9f34f750b
gary1030 <gary6658@ntu.im>
2021-08-29 11:38:13 +0800

    add submission list filter options

--- src/reducers/problems.js
+++ src/reducers/problems.js
@@ -70,9 +70,8 @@ const byId = (state = {}, action) => {
       };
     }
 
-    // this action need to delete all original state, don't revise.
     case commonConstants.FETCH_ALL_CHALLENGES_PROBLEMS_SUCCESS: {
-      console.log(action);
+      // console.log(action);
       const { problems } = action.payload;
       return problems.reduce(
         (acc, item) => ({
@@ -83,7 +82,7 @@ const byId = (state = {}, action) => {
             assistingDataIds: [],
           },
         }),
-        {},
+        state,
       );
     }
 
@@ -125,10 +124,9 @@ const allIds = (state = [], action) => {
     case submissionConstants.READ_PROBLEM_SUCCESS:
       return state.includes(action.payload.id) ? state : state.concat([action.payload.id]);
 
-    // this action need to delete all original state, don't revise.
     case commonConstants.FETCH_ALL_CHALLENGES_PROBLEMS_SUCCESS: {
       const { problems } = action.payload;
-      return problems.map((item) => item.id);
+      return [...new Set([...problems.map((item) => item.id), ...state])];
     }
 
     default:
