cb17e6d5b417b93b8c188cefb018aa40b23227a4
lllulouch <wupochun.2000@gmail.com>
2021-08-30 21:36:17 +0800

    Fix TA icon CSS

--- src/components/ui/sidebar/Challenge.js
+++ src/components/ui/sidebar/Challenge.js
@@ -61,7 +61,7 @@ export default function Challenge({
         && challenges[challengeId] !== undefined
       ) {
         // console.log(problems, essays, userClasses);
-        setTAicon(<Icon.TA className={classNames.titleTA} />);
+        setTAicon(<Icon.TA className={classNames.titleRightIcon} />);
         setArrow(
           <IconButton className={classNames.arrow} onClick={goBackToChallenge}>
             <Icon.ArrowBackRoundedIcon />
@@ -158,7 +158,7 @@ export default function Challenge({
       && problems.byId[problemId] !== undefined
     ) {
       if (userClasses.find((x) => x.class_id === Number(classId)).role === 'MANAGER') {
-        setTAicon(<Icon.TA className={classNames.titleTA} />);
+        setTAicon(<Icon.TA className={classNames.titleRightIcon} />);
       }
       setArrow(
         <IconButton className={classNames.arrow} onClick={goBackToProblem}>
@@ -184,7 +184,7 @@ export default function Challenge({
       && userClasses.find((x) => x.class_id === Number(classId))
     ) {
       if (userClasses.find((x) => x.class_id === Number(classId)).role === 'MANAGER') {
-        setTAicon(<Icon.TA className={classNames.titleTA} />);
+        setTAicon(<Icon.TA className={classNames.titleRightIcon} />);
       }
       setArrow(
         <IconButton className={classNames.arrow} onClick={goBackToSubmission}>
