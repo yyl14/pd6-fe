b076fb9efeab254c5a755a6a867bc323bf5dd168
wendee <wendeehsu@gmail.com>
2021-09-01 12:29:56 +0800

    feat: enable copy on all data

--- src/components/ui/CopyToClipboardButton.js
+++ src/components/ui/CopyToClipboardButton.js
@@ -1,13 +1,6 @@
-import React, { useState, useEffect } from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Card from '@material-ui/core/Card';
-import CardActionArea from '@material-ui/core/CardActionArea';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
+import React from 'react';
 import { CopyToClipboard } from 'react-copy-to-clipboard';
 import { IconButton } from '@material-ui/core';
-import AlignedText from './AlignedText';
 import Icon from './icon/index';
 
 export default function CopyToClipboardButton({ text, onClick }) {
