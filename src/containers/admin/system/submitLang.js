68e220dbdfc5dfa7965e2818847b8684aa16dcf7
gary1030 <gary6658@ntu.im>
2021-09-02 20:48:03 +0800

    admin container refactor done

--- src/containers/admin/system/submitLang.js
+++ src/containers/admin/system/submitLang.js
@@ -1,37 +1,20 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
+import React from 'react';
 
-import { Route, Switch, withRouter } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 
 import SubmissionLanguageHome from '../../../components/admin/system/SubmissionLanguageHome';
 import LangSetting from '../../../components/admin/system/SubmissionLanguageSetting';
 import NoMatch from '../../../components/noMatch';
 
 /* This is a level 3 container (main page container) */
-class SubmitLang extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
-
-  componentDidMount() {}
-
-  render() {
-    return (
-      <>
-        <Switch>
-          <Route path="/admin/system/submitlang/:languageId/setting" component={LangSetting} />
-          <Route exact path="/admin/system/submitlang" component={SubmissionLanguageHome} />
-          <Route component={NoMatch} />
-        </Switch>
-      </>
-    );
-  }
+export default function SubmitLang() {
+  return (
+    <>
+      <Switch>
+        <Route path="/admin/system/submitlang/:languageId/setting" component={LangSetting} />
+        <Route exact path="/admin/system/submitlang" component={SubmissionLanguageHome} />
+        <Route component={NoMatch} />
+      </Switch>
+    </>
+  );
 }
-
-const mapStateToProps = (state) => ({
-  auth: state.auth,
-  error: state.error,
-});
-
-export default connect(mapStateToProps, {})(withRouter(SubmitLang));
