6f15747e35f0cb78c01d106c2792901d3ba14b56
erica0126 <e26cheng@ntu.im>
2021-09-06 20:07:37 +0800

    page title component structure

---
+++ src/components/ui/PageTitle.js
@@ -0,0 +1,18 @@
+import { makeStyles, Typography } from '@material-ui/core';
+import React from 'react';
+
+const useStyles = makeStyles(() => ({
+  title: {
+    wordBreak: 'break-word',
+  },
+}));
+
+export default function PageTitle(text) {
+  const classes = useStyles();
+
+  return (
+    <Typography variant="h3" className={classes.title}>
+      {text}
+    </Typography>
+  );
+}
