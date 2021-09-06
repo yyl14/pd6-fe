5339b8aa9f02b083d4e3c2ece4c9ecaaa2fcff32
erica0126 <e26cheng@ntu.im>
2021-09-06 23:02:22 +0800

    fix bug

--- src/components/ui/PageTitle.js
+++ src/components/ui/PageTitle.js
@@ -8,7 +8,7 @@ const useStyles = makeStyles(() => ({
   },
 }));
 
-export default function PageTitle(text) {
+export default function PageTitle({ text }) {
   const classes = useStyles();
 
   return (
