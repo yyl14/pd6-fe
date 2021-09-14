ab124af6d62cee510f70478476983e1ebfd42583
wendee <wendeehsu@gmail.com>
2021-09-14 15:18:49 +0800

    feat: add fetch problem action

--- src/reducers/error/common/common.js
+++ src/reducers/error/common/common.js
@@ -12,6 +12,7 @@ const initialState = {
   fetchAccount: null,
   fetchAllChallengesProblems: null,
   fetchDownloadFileUrl: null,
+  fetchProblems: null,
 };
 
 export default function common(state = initialState, action) {
@@ -154,6 +155,19 @@ export default function common(state = initialState, action) {
       return {
         ...state,
         fetchDownloadFileUrl: action.error,
+      };
+    }
+
+    case commonConstants.FETCH_PROBLEMS_SUCCESS: {
+      return {
+        ...state,
+        fetchProblems: null,
+      };
+    }
+    case commonConstants.FETCH_PROBLEMS_FAIL: {
+      return {
+        ...state,
+        fetchProblems: action.error,
       };
     }
 
