f6767ece147036b9163ad848d7a795c4552e3077
timicienio <timmy960072@gmail.com>
2021-09-24 17:52:56 +0800

    browse class member reducer

--- src/reducers/classMembers.js
+++ src/reducers/classMembers.js
@@ -1,17 +1,26 @@
 import { combineReducers } from 'redux';
 import { commonConstants } from '../actions/common/constant';
+import { viewConstants } from '../actions/api/constant';
+
+const prototype = {
+  id: null,
+  account_id: null,
+  class_id: null,
+  role: null,
+  institute_abbreviated_name: null,
+};
 
 const byId = (state = {}, action) => {
   switch (action.type) {
-    case commonConstants.FETCH_CLASS_MEMBERS_SUCCESS: {
-      const { data } = action.payload;
-      return data.reduce(
+    case viewConstants.BROWSE_CLASS_MEMBER_SUCCESS: {
+      const { classMembers } = action.payload.data;
+      return classMembers.reduce(
         (acc, item) => ({
           ...acc,
-          [item.member_id]: {
+          [item.id]: {
+            ...prototype,
+            ...state[item.id],
             ...item,
-            member_referral: state[item.member_id] ? state[item.member_id].member_referral : '',
-            member_role: state[item.member_id] ? state[item.member_id].member_role : '',
           },
         }),
         state,
@@ -43,8 +52,11 @@ const byId = (state = {}, action) => {
 
 const allIds = (state = [], action) => {
   switch (action.type) {
-    case commonConstants.FETCH_CLASS_MEMBER_WITH_ACCOUNT_REFERRAL_SUCCESS:
-    case commonConstants.FETCH_CLASS_MEMBERS_SUCCESS: {
+    case viewConstants.BROWSE_CLASS_MEMBER_SUCCESS: {
+      const { classMembers } = action.payload.data;
+      return [...new Set([...classMembers.map((item) => item.member_id), ...state])];
+    }
+    case commonConstants.FETCH_CLASS_MEMBER_WITH_ACCOUNT_REFERRAL_SUCCESS: {
       const { data } = action.payload;
       return [...new Set([...data.map((item) => item.member_id), ...state])];
     }
