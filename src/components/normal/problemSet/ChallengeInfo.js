9864ee1ca136b96dc25a0619280691581d551728
徐遠志 <wendeehsu@gmail.com>
2021-09-24 05:44:27 +0000

    Resolve "Problem Set (Coding Problem Info)"

---
+++ src/components/normal/problemSet/ChallengeInfo.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ChallengeInfoView from '../../ui/templates/ChallengeInfoView';
+
+export default function ChallengeInfo() {
+  return <ChallengeInfoView isManager={false} isProblemSet />;
+}
