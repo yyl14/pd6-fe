5f7ac7429bf4fbcf28a0a0b784964aaa8fd23409
wilson <wilsonhsukdnhc@gmail.com>
2021-09-25 23:41:19 +0800

    peer review div done.

--- src/components/normal/myClass/Challenge/TaskAddingCard.js
+++ src/components/normal/myClass/Challenge/TaskAddingCard.js
@@ -27,6 +27,16 @@ const useStyles = makeStyles(() => ({
   selectedIcon: {
     marginRight: '20px',
   },
+  peerReviewCard_display: {
+    display: 'block',
+  },
+  peerReviewCard_hide: {
+    display: 'none',
+  },
+  peerBottomText: {
+    marginLeft: '10px',
+    marginTop: '18px',
+  },
 }));
 
 /* This is a level 4 component (page component) */
@@ -49,6 +59,12 @@ export default function TaskAddingCard({ open, setOpen }) {
   const [label, setLabel] = useState('');
   const [title, setTitle] = useState('');
   const [disabled, setDisabled] = useState(true);
+  const [PeerReviewCard, setPeerReviewCard] = useState(false);
+  const [challenge, setChallenge] = useState();
+  const [task, setTask] = useState();
+  const [maxScore, setMaxScore] = useState(3);
+  const [minScore, setMinScore] = useState(1);
+  const [peerNumbers, setPeerNumbers] = useState();
 
   useEffect(() => {
     if (!loading.addProblem && !loading.addEssay && !loading.addPeerReview) {
@@ -164,6 +180,79 @@ export default function TaskAddingCard({ open, setOpen }) {
               }}
             />
           </AlignedText>
+          <div
+            className={PeerReviewCard === false ? classNames.peerReviewCard_display : classNames.peerReviewCard_hide}
+          >
+            <hr />
+            <h3>Peer Review Task</h3>
+            <AlignedText text="Challenge" childrenType="field">
+              <FormControl variant="outlined">
+                <Select
+                  labelId="sort"
+                  id="sort"
+                  value={type}
+                  onChange={(e) => {
+                    setChallenge(e.target.value);
+                  }}
+                  style={{ width: '350px' }}
+                >
+                  <MenuItem value="Challenge">
+                    <Icon.Code className={classNames.selectedIcon} />
+                    ChallengeTest
+                  </MenuItem>
+                </Select>
+              </FormControl>
+            </AlignedText>
+            <AlignedText text="Task Label" childrenType="field">
+              <FormControl variant="outlined">
+                <Select
+                  labelId="sort"
+                  id="sort"
+                  value={type}
+                  onChange={(e) => {
+                    setTask(e.target.value);
+                  }}
+                  style={{ width: '350px' }}
+                >
+                  <MenuItem value="Task">
+                    <Icon.Code className={classNames.selectedIcon} />
+                    TaskTest
+                  </MenuItem>
+                </Select>
+              </FormControl>
+            </AlignedText>
+            <AlignedText text="Max Score" childrenType="field">
+              <TextField
+                id="maxScore"
+                value={maxScore}
+                style={{ width: '150px' }}
+                onChange={(e) => {
+                  setMaxScore(e.target.value);
+                }}
+              />
+            </AlignedText>
+            <AlignedText text="Min Score" childrenType="field">
+              <TextField
+                id="minScore"
+                value={minScore}
+                style={{ width: '150px' }}
+                onChange={(e) => {
+                  setMinScore(e.target.value);
+                }}
+              />
+            </AlignedText>
+            <AlignedText text="Student is Assigned" childrenType="field">
+              <TextField
+                id="peerNumbers"
+                value={peerNumbers}
+                style={{ width: '150px' }}
+                onChange={(e) => {
+                  setPeerNumbers(e.target.value);
+                }}
+              />
+              <Typography className={classNames.peerBottomText}>Peers Respectively</Typography>
+            </AlignedText>
+          </div>
         </DialogContent>
         <DialogActions>
           <Button onClick={handleCancel}>Cancel</Button>
