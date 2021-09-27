224f629167f782cf1ee6283370635c4eb8702c84
gary1030 <gary6658@ntu.im>
2021-09-27 21:11:54 +0800

    adjustmnet

--- src/components/normal/myClass/Challenge/PeerReview/PeerReviewSummary.js
+++ src/components/normal/myClass/Challenge/PeerReview/PeerReviewSummary.js
@@ -16,6 +16,27 @@ const useStyles = makeStyles(() => ({
 /* This is a level 4 component (page component) */
 // This page is only for class manager.
 export default function PeerReviewSummary() {
+  const {
+    classId, challengeId, peerReviewId,
+  } = useParams();
+  const classes = useStyles();
+  const dispatch = useDispatch();
+  const [role, setRole] = useState('GUEST');
+
+  const authToken = useSelector((state) => state.auth.token);
+  const loading = useSelector((state) => state.loading.api.peerReviews);
+  const error = useSelector((state) => state.error.api.peerReviews);
+  const commonLoading = useSelector((state) => state.loading.common.common);
+  const userClasses = useSelector((state) => state.user.classes);
+  const challenges = useSelector((state) => state.challenges.byId);
+  const problems = useSelector((state) => state.problem.byId);
+  const peerReviews = useSelector((state) => state.peerReviews.byId);
+  const peerReviewRecords = useSelector((state) => state.peerReviewRecords.byId);
+
+  // if (peerReviews[peerReviewId] === undefined) {
+  //   return <NoMatch />;
+  // }
+
   return (
     <>
       <div>PeerReviewSummary</div>
