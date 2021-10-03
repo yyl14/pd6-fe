a9d87179107ed0923c83b000b7e783e46e9edab4
raymanlee89 <raymanlee89@gmail.com>
2021-10-04 01:00:15 +0800

    Create PeerReviewInfo structure and BasicInfo layout.

--- src/components/normal/myClass/Challenge/PeerReviewSettings/Element/BasicInfo.js
+++ src/components/normal/myClass/Challenge/PeerReviewSettings/Element/BasicInfo.js
@@ -4,21 +4,80 @@ import {
   Typography,
   Button,
   makeStyles,
+  Dialog,
+  DialogTitle,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
 } from '@material-ui/core';
 import { useParams, Link } from 'react-router-dom';
 
 import NoMatch from '../../../../../noMatch';
 import GeneralLoading from '../../../../../GeneralLoading';
 
-const useStyles = makeStyles(() => ({
+import SimpleBar from '../../../../../ui/SimpleBar';
+import AlignedText from '../../../../../ui/AlignedText';
+
+const useStyles = makeStyles((theme) => ({
+  textLink: {
+    textDecoration: 'none',
+    color: theme.palette.primary.main,
+    '&:hover': {
+      color: theme.palette.primary.hover,
+    },
+    '&:active': {
+      color: theme.palette.primary.dark,
+    },
+  },
 }));
 
 /* This is a level 4 component (page component) */
 // This page is for both normal and manager.
-export default function BasicInfo() {
+export default function BasicInfo({ role }) {
+  const classNames = useStyles();
+
+  const handleClickDelete = () => {
+    console.log('delete task');
+  };
+
   return (
     <>
-      <div>BasicInfo</div>
+      <SimpleBar title="Title">Peer Review of Hw3 Q1</SimpleBar>
+      {role === 'MANAGER' && (
+        <SimpleBar title="Description">Description</SimpleBar>
+      )}
+      <SimpleBar title="Peer Review Information">
+        <AlignedText text="Task to be Reviewed" childrenType="text">
+          <Typography variant="body1">
+            <Link to="/" className={classNames.textLink}>
+              <Typography variant="body1">Hw3 / Q1</Typography>
+            </Link>
+          </Typography>
+        </AlignedText>
+        <AlignedText text="Max Score" childrenType="text">
+          <Typography variant="body1">0</Typography>
+        </AlignedText>
+        <AlignedText text="Min Score" childrenType="text">
+          <Typography variant="body1">0</Typography>
+        </AlignedText>
+        <AlignedText text="Student is Assigned" childrenType="text">
+          <Typography variant="body1">2 Peers Respectively</Typography>
+        </AlignedText>
+      </SimpleBar>
+      {role === 'MANAGER' && (
+        <SimpleBar
+          title="Delete Task"
+          childrenButtons={(
+            <>
+              <Button color="secondary" onClick={handleClickDelete}>
+                Delete
+              </Button>
+            </>
+          )}
+        >
+          <Typography variant="body1">Once you delete a task, there is no going back. Please be certain.</Typography>
+        </SimpleBar>
+      )}
     </>
   );
 }
--- src/components/normal/myClass/Challenge/PeerReviewSettings/PeerReviewInfo.js
+++ src/components/normal/myClass/Challenge/PeerReviewSettings/PeerReviewInfo.js
@@ -11,19 +11,73 @@ import BasicInfo from './Element/BasicInfo';
 import Overview from './Element/Overview';
 import NoMatch from '../../../../noMatch';
 import GeneralLoading from '../../../../GeneralLoading';
+import PeerReviewEdit from './PeerReviewEdit';
+
+import PageTitle from '../../../../ui/PageTitle';
+import SimpleBar from '../../../../ui/SimpleBar';
 
 const useStyles = makeStyles(() => ({
+  generalButtons: {
+    display: 'flex',
+    justifyContent: 'flex-end',
+  },
+  managerButtons: {
+    display: 'flex',
+    justifyContent: 'flex-start',
+  },
 }));
+
+// http://localhost:3000/my-class/1/1/challenge/1/peer-review/1
 
 /* This is a level 4 component (page component) */
 // This page is for both normal and manager.
 // Render different component according to role and call correct api (PeerReviewEdit, BasicInfo, Overview)
 export default function PeerReviewInfo() {
+  const { classId, challengeId } = useParams();
+  const classNames = useStyles();
+
+  const userClasses = useSelector((state) => state.user.classes);
+  const challenges = useSelector((state) => state.challenges.byId);
+
+  const [role, setRole] = useState('Normal');
+  const [edit, setEdit] = useState(false);
+
+  const handleView = () => {
+    console.log('View Peer Review');
+  };
+
+  useEffect(() => {
+    if (userClasses.filter((item) => item.class_id === Number(classId)).length !== 0) {
+      setRole(userClasses.filter((item) => item.class_id === Number(classId))[0].role);
+    }
+  }, [classId, userClasses]);
+
   return (
     <>
-      <div>PeerReviewInfo</div>
-      <BasicInfo />
-      <Overview />
+      <PageTitle text={challenges[challengeId] === undefined ? 'error' : `${challenges[challengeId].title} / PR`} />
+      {role === 'MANAGER' ? (
+        <>
+          {!edit && (
+            <div className={classNames.managerButtons}>
+              <Button onClick={() => setEdit(true)}>Edit</Button>
+              <Button onClick={handleView}>View Peer Review</Button>
+            </div>
+          )}
+        </>
+      ) : (
+        <div className={classNames.generalButtons}>
+          <Button variant="outlined" onClick={handleView}>My Peer Reviews</Button>
+          <Button color="primary" onClick={handleView}>Peer Review</Button>
+        </div>
+      )}
+      {edit ? (
+        <PeerReviewEdit />
+      ) : (
+        <>
+          <BasicInfo role={role} />
+          {role !== 'MANAGER' && <Overview />}
+        </>
+      )}
     </>
   );
 }
