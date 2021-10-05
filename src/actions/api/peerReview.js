acbd4e2b34292891edd2f5ed6a89bac15d616919
gary1030 <gary6658@ntu.im>
2021-10-05 22:21:55 +0800

    add some api

--- src/actions/api/peerReview.js
+++ src/actions/api/peerReview.js
@@ -40,29 +40,22 @@ export const deletePeerReview = (token, peerReviewId) => async (dispatch) => {
   }
 };
 
-// export const readPeerReviewRecord = (token, peerReviewRecordId) => async (dispatch) => {
-//   try {
-//     const config = { headers: { 'auth-token': token } };
-//     dispatch({ type: peerReviewConstants.READ_PEER_REVIEW_RECORD_START });
-//     const res = await agent.get(`peer-review-record/${peerReviewRecordId}`, config);
-//     if (res.data.data.grader_id !== null || res.data.data.grader_id !== undefined) {
-//       dispatch(readAccount(token, res.data.data.grader_id));
-//     }
-//     if (res.data.data.receiver_id !== null || res.data.data.receiver_id !== undefined) {
-//       dispatch(readAccount(token, res.data.data.receiver_id));
-//     }
-
-//     dispatch({
-//       type: peerReviewConstants.READ_PEER_REVIEW_RECORD_SUCCESS,
-//       payload: res.data.data,
-//     });
-//   } catch (error) {
-//     dispatch({
-//       type: peerReviewConstants.READ_PEER_REVIEW_RECORD_FAIL,
-//       error,
-//     });
-//   }
-// };
+export const readPeerReviewRecord = (token, peerReviewRecordId) => async (dispatch) => {
+  try {
+    const config = { headers: { 'auth-token': token } };
+    dispatch({ type: peerReviewConstants.READ_PEER_REVIEW_RECORD_START });
+    const res = await agent.get(`peer-review-record/${peerReviewRecordId}`, config);
+    dispatch({
+      type: peerReviewConstants.READ_PEER_REVIEW_RECORD_SUCCESS,
+      payload: res.data.data,
+    });
+  } catch (error) {
+    dispatch({
+      type: peerReviewConstants.READ_PEER_REVIEW_RECORD_FAIL,
+      error,
+    });
+  }
+};
 
 export const browseAccountReviewedPeerReviewRecord = (token, peerReviewId, accountId) => async (dispatch) => {
   try {
