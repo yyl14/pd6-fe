6d6aa19433549a89b1144cc53b34efa487ed8cac
timicienio <timmy960072@gmail.com>
2021-12-09 17:15:52 +0800

    Change default return value of transform functions to empty strings

--- src/components/normal/allClass/Challenge/SubmissionDetail.js
+++ src/components/normal/allClass/Challenge/SubmissionDetail.js
@@ -93,7 +93,7 @@ export default function SubmissionDetail() {
       if (testcases.byId[id].output_filename !== null) {
         return testcases.byId[id].output_filename.slice(6, testcases.byId[id].output_filename.indexOf('.'));
       }
-      return 0;
+      return '';
     },
     [testcases],
   );
@@ -106,7 +106,7 @@ export default function SubmissionDetail() {
       if (testcases.byId[id].output_filename !== null) {
         return testcases.byId[id].output_filename.slice(0, testcases.byId[id].output_filename.indexOf('.'));
       }
-      return 0;
+      return '';
     },
     [testcases],
   );
