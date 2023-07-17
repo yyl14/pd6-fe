d5b49f892fd5036d5470c20d2f87ab5f9bc1b236
gary1030 <gary6658@ntu.im>
2023-07-17 21:03:59 +0800

    feat: remove all class pages

--- src/containers/normal/index.js
+++ src/containers/normal/index.js
@@ -3,7 +3,6 @@ import { useSelector } from 'react-redux';
 
 import { Route, Switch, useHistory } from 'react-router-dom';
 
-import AllClass from './allClass';
 import MyClass from './myClass';
 import ProblemSet from './problemSet';
 
@@ -31,8 +30,6 @@ export default function Normal() {
       {/* For redirection */}
       <Route exact path="/my-class" component={MyClass} />
       <Route path="/my-class/:courseId/:classId" component={MyClass} />
-      {/* Remove all-class link after transition period */}
-      <Route path="/all-class" component={AllClass} />
       <Route exact path="/problem-set" component={ProblemSet} />
       <Route path="/problem-set/:courseId/:classId" component={ProblemSet} />
       <Route exact path="/" component={GeneralLoading} />
