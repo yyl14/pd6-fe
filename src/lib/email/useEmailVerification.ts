8d62e076810e789786f1e5ea40a42da23b87e69f
李昀宸 <b09705015@ntu.edu.tw>
2023-07-23 17:12:10 +0800

    refactor: rewrite /email-verification/{emailVerificationId}/resend with custom hooks

---
+++ src/lib/email/useEmailVerification.ts
@@ -0,0 +1,30 @@
+import useSWRMutation from 'swr/mutation';
+import toSWRFetcher from '../../function/toSWRMutationFetcher';
+import { deletePendingEmailVerification, resendEmailVerification } from './fetchers';
+
+const useEmailVerification = (emailVerificationId: number) => {
+  const resendEmailVerificationSWR = useSWRMutation(
+    `/email-verification/${emailVerificationId}/resend`,
+    toSWRFetcher(() => resendEmailVerification({ email_verification_id: emailVerificationId })),
+  );
+  const deletePendingEmailVerificationSWR = useSWRMutation(
+    `/email-verification/${emailVerificationId}`,
+    toSWRFetcher(() => deletePendingEmailVerification({ email_verification_id: emailVerificationId })),
+  );
+
+  return {
+    resendEmailVerification: resendEmailVerificationSWR.trigger,
+    deletePendingEmailVerification: deletePendingEmailVerificationSWR.trigger,
+    isLoading: {
+      resendEmailVerification: resendEmailVerificationSWR.isMutating,
+      deletePendingEmailVerification: deletePendingEmailVerificationSWR.isMutating,
+    },
+    error: {
+      resendEmailVerification: resendEmailVerificationSWR.error ?? resendEmailVerificationSWR.data?.data.error,
+      deletePendingEmailVerification:
+        deletePendingEmailVerificationSWR.error ?? deletePendingEmailVerificationSWR.data?.data.error,
+    },
+  };
+};
+
+export default useEmailVerification;
