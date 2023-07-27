9e9059a65a4994c34cd9cad4f8d65614a5ba38a9
Claire <Claire@MacBook-Pro-2.local>
2023-07-27 14:58:05 +0800

    chore: add hooks

---
+++ src/lib/course/useCourses.ts
@@ -0,0 +1,24 @@
+import useSWR from 'swr';
+import useSWRMutation from 'swr/mutation';
+import toSWRMutationFetcher from '../../function/toSWRMutationFetcher';
+import { addCourse, browseAllCourse } from './fetchers';
+
+const useCourses = () => {
+  const browseAllCourseSWR = useSWR('/course', () => browseAllCourse({}));
+  const addCourseSWR = useSWRMutation('/course', toSWRMutationFetcher(addCourse));
+
+  return {
+    courses: browseAllCourseSWR.data?.data.data,
+    addInstitute: addCourseSWR.trigger,
+    isLoading: {
+      browseAll: browseAllCourseSWR.isLoading,
+      add: addCourseSWR.isMutating,
+    },
+    error: {
+      browseAll: browseAllCourseSWR.error,
+      add: addCourseSWR.error,
+    },
+  };
+};
+
+export default useCourses;
