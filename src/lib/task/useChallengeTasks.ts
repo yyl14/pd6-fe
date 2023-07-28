250c20b2f12946041a037ab5c2b265c13aa32882
miachou <cyr1225@ntu.im>
2023-07-28 16:40:18 +0800

    feat: add hooks

---
+++ src/lib/task/useChallengeTasks.ts
@@ -0,0 +1,47 @@
+import useSWR from 'swr';
+import useSWRMutation from 'swr/mutation';
+import toSWRMutationFetcher from '../../function/toSWRMutationFetcher';
+import { readTask, addTaskProblem, addTaskEssay, addTaskPeerReview, addTaskTeamProjectScoreboard } from './fetchers';
+
+const useChallengeTasks = (challengeId: number) => {
+    const readTaskSWR = useSWR(`/challenge/${challengeId}/task`, 
+        () => readTask({ challenge_id: challengeId }));
+  
+    const addTaskProblemSWR = useSWRMutation(`/challenge/${challengeId}/problem`, 
+          toSWRMutationFetcher(addTaskProblem));
+
+    const addTaskEssaySWR = useSWRMutation(`/challenge/${challengeId}/problem`, 
+          toSWRMutationFetcher(addTaskEssay));
+    
+    const addTaskPeerReviewSWR = useSWRMutation(`/challenge/${challengeId}/problem`, 
+          toSWRMutationFetcher(addTaskPeerReview));
+
+    const addTaskTeamProjectScoreboardSWR = useSWRMutation(`/challenge/${challengeId}/problem`, 
+          toSWRMutationFetcher(addTaskTeamProjectScoreboard));
+  
+    return {
+        task: readTaskSWR.data?.data.data,
+        addTaskProblem: addTaskProblemSWR.trigger,
+        addTaskEssay: addTaskEssaySWR.trigger,
+        addTaskPeerReview: addTaskPeerReviewSWR.trigger,
+        addTaskTeamProjectScoreboard: addTaskTeamProjectScoreboardSWR.trigger,
+  
+        isLoading: {
+            read: readTaskSWR.isLoading,
+            addProblem: addTaskProblemSWR.isMutating,
+            addEssay: addTaskEssaySWR.isMutating,
+            addPeerReview: addTaskPeerReviewSWR.isMutating,
+            addTeamProjectScoreboard: addTaskTeamProjectScoreboardSWR.isMutating,
+        },
+  
+        error: {
+            read: readTaskSWR.error,
+            addProblem: addTaskProblemSWR.error,
+            addEssay: addTaskEssaySWR.error,
+            addPeerReview: addTaskPeerReviewSWR.error,
+            addTeamProjectScoreboard: addTaskTeamProjectScoreboardSWR.error,
+        },
+    };
+};
+  
+export default useChallengeTasks;
