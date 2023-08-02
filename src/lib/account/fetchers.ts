eceebba8212d936cb772bb28666948c407fe5cea
李昀宸 <b09705015@ntu.edu.tw>
2023-08-03 04:56:43 +0800

    refactor: rewrite /account/{account_id} with custom hooks

--- src/lib/account/fetchers.ts
+++ src/lib/account/fetchers.ts
@@ -1,4 +1,9 @@
 import api from '../api';
 
-// eslint-disable-next-line import/prefer-default-export
 export const getAccountTemplateFile = api.path('/account/template').method('get').create();
+
+export const readAccountWithDefaultStudentId = api.path('/account/{account_id}').method('get').create();
+
+export const deleteAccount = api.path('/account/{account_id}').method('delete').create();
+
+export const editAccount = api.path('/account/{account_id}').method('patch').create();
