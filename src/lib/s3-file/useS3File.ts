140511e4df6dfe5536dae78058a54f59efb5295a
miachou <cyr1225@ntu.im>
2023-08-02 21:30:47 +0800

    feat: add error handling

--- src/lib/s3-file/useS3File.ts
+++ src/lib/s3-file/useS3File.ts
@@ -3,36 +3,47 @@ import toSWRMutationFetcher from '../../function/toSWRMutationFetcher';
 import getFileLink from './fetchers';
 import getTextFromUrl from '../../function/getTextFromUrl';
 
+type OnErrorType = () => void | null;
+
 const useS3File = (uuid: string) => {
     const getFileLinkSWR = useSWRMutation(`/s3-file/${uuid}/url`, 
         toSWRMutationFetcher(getFileLink));
 
-    const downloadFile = async (fileName: string, file_uuid: string, asAttachment: boolean) => {
-        const res = getFileLink({
-            s3_file_uuid: file_uuid, 
-            filename: fileName, 
-            as_attachment: asAttachment
-        })
-        if((await res).ok) {
+    const downloadFile = async (fileName: string, file_uuid: string, asAttachment: boolean, onError: OnErrorType) => {
+        try {
+            const res = getFileLink({
+                s3_file_uuid: file_uuid, 
+                filename: fileName, 
+                as_attachment: asAttachment
+            })
             fetch((await res).data.data.url).then((t) =>
-            t.blob().then((b) => {
-                const a = document.createElement('a');
-                a.href = URL.createObjectURL(b);
-                a.setAttribute('download', fileName);
-                a.click();
-            }),
-        )}
+                t.blob().then((b) => {
+                    const a = document.createElement('a');
+                    a.href = URL.createObjectURL(b);
+                    a.setAttribute('download', fileName);
+                    a.click();
+                }),
+            )
+        } catch(err) {
+            if(onError){
+                onError();
+            }
+        }
     }
 
     const getFileContent = async (fileName: string, file_uuid: string, asAttachment: boolean) => {
-        const res = getFileLink({
-            s3_file_uuid: file_uuid, 
-            filename: fileName, 
-            as_attachment: asAttachment
-        })
-        const { data } = (await res).data;
-        const code = await getTextFromUrl(data.url);
-        return code;
+        try {
+            const res = getFileLink({
+                s3_file_uuid: file_uuid, 
+                filename: fileName, 
+                as_attachment: asAttachment
+            })
+            const { data } = (await res).data;
+            const code = await getTextFromUrl(data.url);
+            return code;
+        } catch(err) {
+            return null;
+        }
     }
   
     return {
