0fbde2126b020213bee14a91077bbc6324c3a0da
ansonWang0125 <b09705044@ntu.edu.tw>
2023-08-04 15:17:22 +0800

    fix: sonarqube empty before script and lighthouse assert setting modify

--- lighthouserc.js
+++ lighthouserc.js
@@ -16,7 +16,25 @@ module.exports = {
         },
         assert: {
             // assert options here
-            preset: "lighthouse:recommended",
+            // preset: "lighthouse:recommended", // will fail in CI pipeline
+            assertions: {
+                'categories:performance': [
+                  'error',
+                  { minScore: 0.9, aggregationMethod: 'median-run' },
+                ],
+                'categories:accessibility': [
+                  'error',
+                  { minScore: 1, aggregationMethod: 'pessimistic' },
+                ],
+                'categories:best-practices': [
+                  'error',
+                  { minScore: 1, aggregationMethod: 'pessimistic' },
+                ],
+                'categories:seo': [
+                  'error',
+                  { minScore: 1, aggregationMethod: 'pessimistic' },
+                ],
+              },
         },
         upload: {
             // upload options here
