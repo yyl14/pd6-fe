678315ddc969edf13ae75ad9975b3f91cbe8a867
jerrymk <jerrymk@ntu.im>
2023-08-05 23:30:59 +0800

    refactor: added missing hooks

--- src/lib/testcase/fetchers.ts
+++ src/lib/testcase/fetchers.ts
@@ -2,7 +2,14 @@ import api from '../api';
 
 export const browseAllTestcase = api.path('/problem/{problem_id}/testcase').method('get').create();
 export const addTestcase = api.path('/problem/{problem_id}/testcase').method('post').create();
-// all sample/non sample testcase?
+export const downloadAllSampleTestcase = api
+  .path('/problem/{problem_id}/all-sample-testcase')
+  .method('post')
+  .create({ as_attachment: true });
+export const downloadAllNonSampleTestcase = api
+  .path('/problem/{problem_id}/all-non-sample-testcase')
+  .method('post')
+  .create({ as_attachment: true });
 
 // export const readTestcase = api.path('/testcase/{testcase_id}').method('get').create();
 export const deleteTestcase = api.path('/testcase/{testcase_id}').method('delete').create();
