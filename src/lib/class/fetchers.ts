be805be70f110667ea28accb5a7bff0b75ce971b
李昀宸 <liyunchen@liyunchendeMacBook-Air.local>
2023-08-05 18:04:37 +0800

    refactor: rewrite /course/{course_id}/class with cusstom hooks

--- src/lib/class/fetchers.ts
+++ src/lib/class/fetchers.ts
@@ -9,3 +9,7 @@ export const editClass = api.path('/class/{class_id}').method('patch').create();
 export const browseAllAccountWithClassRole = api.path('/account/{account_id}/class').method('get').create();
 
 export const browseClass = api.path('/class').method('get').create();
+
+export const browseAllClassUnderCourse = api.path('/course/{course_id}/class').method('get').create();
+
+export const addClassUnderCourse = api.path('/course/{course_id}/class').method('post').create();
