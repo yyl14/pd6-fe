38d94d2f0426db98f85200e3850700dcc446f935
李昀宸 <liyunchen@liyunchendeMacBook-Air.local>
2023-08-05 17:34:08 +0800

    refactor: rewrite /class with cusstom hooks

--- src/lib/class/fetchers.ts
+++ src/lib/class/fetchers.ts
@@ -7,3 +7,5 @@ export const deleteClass = api.path('/class/{class_id}').method('delete').create
 export const editClass = api.path('/class/{class_id}').method('patch').create();
 
 export const browseAllAccountWithClassRole = api.path('/account/{account_id}/class').method('get').create();
+
+export const browseClass = api.path('/class').method('get').create();
