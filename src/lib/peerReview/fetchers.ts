86965bbd7874419421963db616bdd895fb8f6585
李昀宸 <liyunchen@liyunchendeMacBook-Air.local>
2023-08-06 01:16:16 +0800

    refactor: rewrite /peer-review-record/{peer_review_record_id} with cusstom hooks

--- src/lib/peerReview/fetchers.ts
+++ src/lib/peerReview/fetchers.ts
@@ -9,3 +9,7 @@ export const editPeerReview = api.path('/peer-review/{peer_review_id}').method('
 export const browsePeerReviewRecord = api.path('/peer-review/{peer_review_id}/record').method('get').create();
 
 export const assignPeerReviewRecord = api.path('/peer-review/{peer_review_id}/record').method('post').create();
+
+export const readPeerReviewRecord = api.path('/peer-review-record/{peer_review_record_id}').method('get').create();
+
+export const submitPeerReviewRecord = api.path('/peer-review-record/{peer_review_record_id}').method('patch').create();
