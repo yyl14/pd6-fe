8bfc8a2bf225ddea77cfaafb5d7b0f354a7a19a0
gary1030 <gary6658@ntu.im>
2023-08-06 20:58:18 +0800

    chore: update assisting data fetcher

--- src/lib/assistingData/fetchers.ts
+++ src/lib/assistingData/fetchers.ts
@@ -3,3 +3,7 @@ import api from '../api';
 export const browseAssistingData = api.path('/problem/{problem_id}/assisting-data').method('get').create();
 
 export const addAssistingData = api.path('/problem/{problem_id}/assisting-data').method('post').create();
+
+export const editAssistingData = api.path('/assisting-data/{assisting_data_id}').method('put').create();
+
+export const deleteAssistingData = api.path('/assisting-data/{assisting_data_id}').method('delete').create();
