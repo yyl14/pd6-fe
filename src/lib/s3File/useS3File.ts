4881a446ffdb1f389100939bdfbba870d6876aba
gary1030 <gary6658@ntu.im>
2023-08-07 15:13:37 +0800

    fix: await download file

--- src/lib/s3File/useS3File.ts
+++ src/lib/s3File/useS3File.ts
@@ -3,39 +3,26 @@ import getTextFromUrl from '../../function/getTextFromUrl';
 import toSWRMutationFetcher from '../../function/toSWRMutationFetcher';
 import getFileLink from './fetchers';
 
-type OnErrorType = () => void;
-
 const useS3File = () => {
   const getFileLinkSWR = useSWRMutation(`/s3-file/{uuid}/url`, toSWRMutationFetcher(getFileLink));
 
-  const downloadFile = async (
-    fileName: string,
-    file_uuid: string,
-    asAttachment: boolean = true,
-    onError: OnErrorType | null = null,
-  ) => {
-    try {
-      const { data } = await getFileLinkSWR.trigger({
-        s3_file_uuid: file_uuid,
-        filename: fileName,
-        as_attachment: asAttachment,
-      });
-      fetch(data.data.url).then((t) =>
-        t.blob().then((b) => {
-          const a = document.createElement('a');
-          a.href = URL.createObjectURL(b);
-          a.setAttribute('download', fileName);
-          a.click();
-        }),
-      );
-    } catch (err) {
-      if (onError) {
-        onError();
-      }
-    }
+  const downloadFile = async (fileName: string, file_uuid: string, asAttachment = true) => {
+    const { data } = await getFileLinkSWR.trigger({
+      s3_file_uuid: file_uuid,
+      filename: fileName,
+      as_attachment: asAttachment,
+    });
+
+    const res = await fetch(data.data.url);
+    const blob = await res.blob();
+
+    const a = document.createElement('a');
+    a.href = URL.createObjectURL(blob);
+    a.setAttribute('download', fileName);
+    a.click();
   };
 
-  const getFileContent = async (fileName: string, file_uuid: string, asAttachment: boolean = true) => {
+  const getFileContent = async (fileName: string, file_uuid: string, asAttachment = true) => {
     try {
       const { data } = await getFileLinkSWR.trigger({
         s3_file_uuid: file_uuid,
