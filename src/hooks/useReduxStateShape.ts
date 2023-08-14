c304ed60a29d98ca4f43fd8fde91181bebacc4b0
gary1030 <gary6658@ntu.im>
2023-08-14 11:35:24 +0800

    fix: useReduxStateShape type error

--- src/hooks/useReduxStateShape.ts
+++ src/hooks/useReduxStateShape.ts
@@ -3,7 +3,9 @@ import { useMemo } from 'react';
 /**
  * Utility that converts data shape returned by SWR into Redux state shape.
  */
-const useReduxStateShape = <T extends { id: string }>(arr: T[] | undefined) => {
+const useReduxStateShape = <T extends { id: string | number }>(
+  arr: T[] | undefined,
+): [Record<string | number, T>, Array<string | number>] => {
   const byId = useMemo(() => arr?.reduce((acc, item) => ({ ...acc, [item.id]: item }), {}) ?? {}, [arr]);
   const ids = useMemo(() => arr?.map((item) => item.id) ?? [], [arr]);
 
