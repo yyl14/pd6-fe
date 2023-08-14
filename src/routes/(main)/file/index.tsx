2c5a221003c20abffc258fa3f8ea44d793b49d75
gary1030 <gary6658@ntu.im>
2023-08-14 12:52:23 +0800

    refactor: file downloading page

--- src/routes/(main)/file/index.tsx
+++ src/routes/(main)/file/index.tsx
@@ -1,11 +1,23 @@
-import { Suspense, lazy } from 'react';
+import { Suspense, lazy, useMemo } from 'react';
+import { useHistory } from 'react-router-dom';
+
+import useQuery from '@/hooks/useQuery';
 
 const FileDownload = lazy(() => import(/* webpackChunkName: "FileDownload" */ '@/pages/FileDownloading'));
 
 export default function FileDownloading() {
+  const history = useHistory();
+  const query = useQuery();
+  const filename = useMemo<string>(() => query.get('filename') as string, [query]);
+  const uuid = useMemo<string>(() => query.get('uuid') as string, [query]);
+
+  if (filename === null || uuid === null) {
+    history.push('/');
+  }
+
   return (
     <Suspense fallback={<></>}>
-      <FileDownload />
+      <FileDownload filename={filename} uuid={uuid} />
     </Suspense>
   );
 }
