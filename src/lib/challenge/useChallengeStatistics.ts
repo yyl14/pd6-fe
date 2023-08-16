3ffc7c859364d09805a90c8a974db7449d1fdce9
miachou <cyr1225@ntu.im>
2023-08-16 22:34:24 +0800

    feat: add download submission and plagiarism hooks

--- src/lib/challenge/useChallengeStatistics.ts
+++ src/lib/challenge/useChallengeStatistics.ts
@@ -1,16 +1,40 @@
 import useSWR from 'swr';
-import { getChallengeStatistics } from './fetchers';
+import useSWRMutation from 'swr/mutation';
+
+import toSWRMutationFetcher from '@/function/toSWRMutationFetcher';
+
+import { downloadAllPlagiarismReports, downloadAllSubmissions, getChallengeStatistics } from './fetchers';
 
 const useChallengeStatistics = (challengeId: number) => {
   const getChallengeStatisticsSWR = useSWR(`/challenge/${challengeId}/statistics/summary`, () =>
     getChallengeStatistics({ challenge_id: challengeId }),
   );
+  const downloadAllSubmissionsSWR = useSWRMutation(
+    `/challenge/${challengeId}/all-submission`,
+    toSWRMutationFetcher(downloadAllSubmissions),
+  );
+
+  const downloadAllPlagiarismReportsSWR = useSWRMutation(
+    `/challenge/${challengeId}/all-plagiarism-report`,
+    toSWRMutationFetcher(downloadAllPlagiarismReports),
+  );
 
   return {
     challengeStatistics: getChallengeStatisticsSWR.data?.data.data,
+    downloadAllSubmissions: downloadAllSubmissionsSWR.trigger,
+    downloadAllPlagiarismReports: downloadAllPlagiarismReportsSWR.trigger,
 
-    isLoading: getChallengeStatisticsSWR.isLoading,
-    error: getChallengeStatisticsSWR.error,
+    isLoading: {
+      getStatistics: getChallengeStatisticsSWR.isLoading,
+      downloadSubmissions: downloadAllSubmissionsSWR.isMutating,
+      downloadPlagiarisms: downloadAllPlagiarismReportsSWR.isMutating,
+    },
+
+    error: {
+      getStatistics: getChallengeStatisticsSWR.error,
+      downloadSubmissions: downloadAllSubmissionsSWR.error,
+      downloadPlagiarisms: downloadAllPlagiarismReportsSWR.error,
+    },
   };
 };
 
