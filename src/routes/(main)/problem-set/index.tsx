305423cda082364d8d8172f7fed478a5f820ed61
李昀宸 <liyunchen@liyunchendeAir.home>
2023-08-23 12:46:19 +0800

    fix: add loading states

--- src/routes/(main)/problem-set/index.tsx
+++ src/routes/(main)/problem-set/index.tsx
@@ -6,6 +6,8 @@ import withConditionalRender from '@/components/hoc/withConditionalRender';
 import useChallenge from '@/lib/challenge/useChallenge';
 import useClass from '@/lib/class/useClass';
 import useCourse from '@/lib/course/useCourse';
+import useProblemScores from '@/lib/problem/useProblemScores';
+import useViewClassProblemSets from '@/lib/view/useViewClassProblemSets';
 import useProblemSetMiddleware from '@/middleware/useProblemSetMiddleware';
 
 const ChallengeInfo = lazy(() => import('@/pages/ChallengeInfo'));
@@ -32,13 +34,18 @@ function ChallengeInfoRoute() {
 
 function ProblemListRoute() {
   const { courseId, classId } = useParams<{ courseId: string; classId: string }>();
-
+  const { isLoading: courseLoading } = useCourse(Number(courseId));
+  const { isLoading: classLoading } = useClass(Number(classId));
+  const { browseProblemSetUnderClass, isLoading: classProblemLoading } = useViewClassProblemSets(Number(classId));
+  const { isLoading: problemScoresLoading } = useProblemScores(
+    browseProblemSetUnderClass.data?.data.map((problem) => problem.problem_id) ?? [],
+  );
   return (
     <Suspense fallback={<GeneralLoading />}>
       {withConditionalRender(ProblemList)({
         courseId,
         classId,
-        isLoading: false,
+        isLoading: courseLoading.read || classLoading.read || classProblemLoading.browse || problemScoresLoading.browse,
       })}
     </Suspense>
   );
